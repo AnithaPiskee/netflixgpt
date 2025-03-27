78f6b431da13bd9d9e00ee113cd314b9e9b86f54
AnithaPiske <anithasangameshwar444@gmail.com>
2025-03-27 17:24:50 +0530

    setup parcel & tailwindcss

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 const App = () => {
-  return <h1>Hello, React with Parcel and Babel!</h1>;
+  return <h1 className='text-4xl text-amber-300'>Hello, React with Parcel and Babel!</h1>;
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
